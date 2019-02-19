854ed0ce5febf76d8781c6a9fc9a61412c8b779f
Alex Michael <alexpmichael7@gmail.com>
2019-02-19 13:42:19 -0600

    Finish draft of html_in_jpg_ie.py

--- python-code/html_in_jpg_ie.py
+++ python-code/html_in_jpg_ie.py
@@ -6,6 +6,7 @@ Creates and HTML+JPG polyglot for Internet Explorer
 
 import argparse
 import re
+import random
 
 RANDOM_DATA_SIZE = 1900
 
@@ -28,6 +29,13 @@ STEGO = """
                               `~---o.       `~___o.---'
 """
 
+
+def genRandomHTMLChar():
+	while True:
+		x = random.randint(0,255)
+		if x != 0 and x!=ord('<') and x!=ord('>') and x!=ord('/'):
+			return x
+
 def main():
 	print(STEGO)	# print the stego
 
@@ -35,7 +43,7 @@ def main():
 	parser = argparse.ArgumentParser(description = description)
 	parser.add_argument('htmlfile', 
 			type=argparse.FileType('r'),
-			nargs='?',					# REMOVE ME
+			nargs='?',					# REMOVE ME: sets the arg optional
 			help='HTML file name')
 
 	parser.add_argument('jpgfile', 
@@ -76,14 +84,35 @@ def main():
 	metaTags = re.compile('<meta http-equiv[^>]*>')
 
 	htmlData = re.sub(htmlTags,'',htmlData)
-	htmlData = re.sub(headTags,'',htmlData)
-	htmlData = re.sub(metaTags,'',htmlData)
+	# htmlData = re.sub(headTags,'',htmlData)
+	# htmlData = re.sub(metaTags,'',htmlData)
 
 	content += htmlData + '<!--'
 
-	##################################
-	##		BOOKMARK LINE 94		##
-	##################################
+	paddingLength = 0x2f2a - len(content) - len(html)
+	randomLength = random.randint(0,RANDOM_DATA_SIZE)
+
+	padding = ''
+	for i in range(paddingLength):
+		x = genRandomHTMLChar()
+		padding += chr(x)
+
+	prePadding = padding[0:randomLength]
+	postPadding = padding[randomLength:]
+	
+	finalContent = html + prePadding + content + postPadding	
+
+	# lots of print statements
+	print('HTML header length = %d' % len(html))
+	print('Pre padding length = %d' % len(prePadding))
+	print('Post padding length = %d' % len(postPadding))
+	print('HTML data length = %d' % len(htmlData))
+	print('Content length = %d' % len(content))
+	print('Final content length = %d' % len(finalContent))
+
+	newJpg = jpgStart + '/*'.encode() + jfifapp0 + finalContent.encode() + restOfJpg + '*/ -->'.encode()
+
+	print(newJpg)		# Output to file
 
 if __name__ == '__main__':
 	main()
